3c0a895037c1342ee3d5ea43da2d21a1bd76670e
ZachHung <zachhung911@gmail.com>
2023-01-11 20:41:53 +0700

    fix routing issue

--- routes.js
+++ routes.js
@@ -7,11 +7,8 @@ const router = Router();
 const route = (app) => {
   app.use(
     '/telegram',
-    router.post('', (req, res) => 
-      ChatService.sendMessage(req, res);
-    }),
+    router.post('', (req, res) => ChatService.sendMessage(req, res)),
   );
-
   if (process.env.NODE_ENV !== 'development') {
     app.use('/bot', webhookCallback(bot, 'express'));
   }
