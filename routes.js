d80dbdc73960745942cdc509932f47835af40ffe
longtruong2604 <longtruong2604.@gmail.com>
2023-01-11 16:56:01 +0700

    Long's first commit

--- routes.js
+++ routes.js
@@ -1,11 +1,18 @@
-import ChatService from './services.js';
-import { Router } from 'express';
+import ChatService from "./services.js";
+import { Router } from "express";
 const router = Router();
 const route = (app) => {
   app.use(
-    '/telegram/',
-    router.post('', (req, res) => {
+    "/telegram/",
+    router.post("", (req, res) => {
       ChatService.sendMessage(req, res);
+    })
+  );
+
+  app.use(
+    "/test/",
+    router.get("", (req, res) => {
+      ChatService.createNotionPage(req, res);
     })
   );
 };
--- services.js
+++ services.js
@@ -1,9 +1,77 @@
-import { bot } from './app.js';
+import { bot } from "./app.js";
+import { notion } from "./app.js";
 class ChatService {
   sendMessage(req, res) {
-    let message = `${req.body.page.properties['Tên (hoặc FB)'].title[0].text.content} đã đến hạn`;
+    let message = `${req.body.page.properties["Tên (hoặc FB)"].title[0].text.content} đã đến hạn`;
     bot.telegram.sendMessage(process.env.CHAT_ID, message);
     res.status(200).json(message);
+  }
+
+  async createNotionPage(req, res) {
+    try {
+      let result = await notion.pages.create({
+        parent: {
+          database_id: process.env.NOTION_DATABASE_ID,
+        },
+        properties: {
+          "Tên (hoặc FB)": {
+            title: [
+              {
+                text: {
+                  content: "Dummy Name",
+                },
+              },
+            ],
+          },
+
+          Username: {
+            rich_text: [
+              {
+                type: "text",
+                text: {
+                  content: "Dummy username",
+                },
+              },
+            ],
+          },
+
+          Pass: {
+            rich_text: [
+              {
+                text: {
+                  content: "Dummy password",
+                },
+              },
+            ],
+          },
+
+          Slot: {
+            rich_text: [
+              {
+                text: {
+                  content: "Dummy slot name",
+                },
+              },
+            ],
+          },
+
+          Date: {
+            date: {
+              start: "2023-01-01",
+            },
+          },
+
+          "Subcribe?": {
+            select: {
+              name: "3 tháng",
+            },
+          },
+        },
+      });
+      res.send(result);
+    } catch (err) {
+      res.send(err);
+    }
   }
 }
 
