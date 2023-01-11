23d1004901fb0703489b4f9c504ae746668b141f
Thành Hưng <59259431+ZachHung@users.noreply.github.com>
2023-01-11 20:36:17 +0700

    Hung (#3)
    * Long's first commit
    
    * rebuilt using grammY and added new user conversation
    
    * implement husky with lint-staged and prettier
    
    Co-authored-by: longtruong2604 <longtruong2604.@gmail.com>

--- routes.js
+++ routes.js
@@ -1,17 +1,20 @@
 import ChatService from './services.js';
 import { Router } from 'express';
 import { webhookCallback } from 'grammy';
+import bot from './bot.js';
+
 const router = Router();
 const route = (app) => {
   app.use(
     '/telegram',
-    router.post('', (req, res) => {
+    router.post('', (req, res) => 
       ChatService.sendMessage(req, res);
-    })
+    }),
   );
 
-  if (process.env.NODE_ENV !== 'development')
+  if (process.env.NODE_ENV !== 'development') {
     app.use('/bot', webhookCallback(bot, 'express'));
+  }
 };
 
 export default route;
